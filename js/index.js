61b9d402764351e1ce6a40218737007f0d239b98
abdowahid <abdowahid2014@gmail.com>
2017-05-24 21:13:21 +0200

    fds

--- js/index.js
+++ js/index.js
@@ -65,3 +65,31 @@ document.addEventListener('DOMContentLoaded', function(){
     function newTyped(){ /* A new typed object */ }
 
     function foo(){ console.log("Callback"); }
+
+$(function () {
+
+    var filterList = {
+
+        init: function () {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixItUp({
+                selectors: {
+                    target: '.portfolio',
+                    filter: '.filter'
+                },
+                load: {
+                    filter: '.app'
+                }
+            });
+
+        }
+
+    };
+
+    // Run the show!
+    filterList.init();
+
+
+});
